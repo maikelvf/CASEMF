4265c9aa66b93621d5dde751d51d912788702e29
Maikel van Faassen <maikelvanfaassen@hotmail.com>
2020-07-02 20:06:49 +0200

    e2e testen toegevoegd

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,34 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Cursusadministratie', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('frontend app is running!');
+    expect(page.getTitleText()).toEqual('Cursusadministratie');
+  });
+
+
+  // LET OP: Onderstaande test is afhankelijk van de initiële database staat. Oftewel:
+  // Backend applicatie moet draaiende zijn
+  // Frontend applicatie opent bij week 27 jaar 2020, 4 cursussen zijn zichtbaar.
+  // Weeknummer wordt door de test verandert in 28, 6 cursussen zijn zichtbaar.
+  // Weeknummer wordt door de test verander in 26, 2 cursussen zijn zichtbaar.
+  // Zijn er door de gebruiker cursussen toegevoegd dan zal het aantal waarschijnlijk anders zijn.
+
+  it('should display correct number of cursussen when switching weeknumbers', () => {
+    page.navigateTo();
+    expect(page.getNumberOfCursussen()).toBe(4);
+
+    page.clickIncreaseWeeknumberButton(1);
+    expect(page.getNumberOfCursussen()).toBe(6);
+
+    page.clickDecreaseWeeknumberButton(2);
+    expect(page.getNumberOfCursussen()).toBe(2);
+  })
+
+  it(`should display 'no file selected' error when trying to upload with no file selected`, () => {
+    page.navigateToRoute('/toevoegen');
+    page.clickSubmitButton();
+    expect(page.noFileErrorMessageVisible());
   });
 
   afterEach(async () => {
--- frontend/e2e/src/app.po.ts
+++ frontend/e2e/src/app.po.ts
@@ -1,11 +1,49 @@
 import { browser, by, element } from 'protractor';
+import { __values } from 'tslib';
+import { stringify } from 'querystring';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
+  navigateToRoute(route): Promise<unknown> {
+    return browser.get(route) as Promise<unknown>;
+  }
+
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.id('cursusadministratie')).getText() as Promise<string>;
+  }
+
+  clickSubmitButton(): void {
+    element(by.id('submitButton')).click();
+  }
+
+  getNumberOfCursussen(): Number {
+    var count;
+
+    count = element.all(by.css('.tableRow')).count();
+
+    return count;
+  }
+
+  clickIncreaseWeeknumberButton(n) {
+    for (var i = 0; i < n; i++) {
+      element(by.id('increaseButton')).click();
+    }
+  }
+
+  clickDecreaseWeeknumberButton(n) {
+    for (var i = 0; i < n; i++) {
+      element(by.id('decreaseButton')).click();
+    }
+  }
+
+  noFileErrorMessageVisible(): string {
+    var text;
+
+    element(by.id('errormessage')).getText().then(value => { text = value });
+
+    return text;
   }
 }
